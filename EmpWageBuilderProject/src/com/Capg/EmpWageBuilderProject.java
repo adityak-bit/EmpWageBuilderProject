bad67ecf8d16e9a5f36af6c5c2fb7fada472fa3a
adityak-bit <adityajsr1998@gmail.com>
2020-09-29 12:01:43 +0530

    Added : check employee attendance

--- EmpWageBuilderProject/src/com/Capg/EmpWageBuilderProject.java
+++ EmpWageBuilderProject/src/com/Capg/EmpWageBuilderProject.java
@@ -1,8 +1,21 @@
 package com.Capg;
 
+import java.util.Random;
+
 public class EmpWageBuilderProject {
+
+	public static final int IS_FULL_TIME = 1;
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+		// TODO Auto-generated method stub
+     
+     Random r = new Random();
+     int empCheck =  r.nextInt(2) ;
+     
+     if(empCheck == IS_FULL_TIME)
+             System.out.println("Employee is Present");
+     else
+    	     System.out.println("Employee is Abscent");
 	}
 
 }
