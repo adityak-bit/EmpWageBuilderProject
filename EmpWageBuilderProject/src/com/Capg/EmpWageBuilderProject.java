6efe55ed4842bfb40ff162a1f71c8690cc2912a2
adityak-bit <adityajsr1998@gmail.com>
2020-09-29 12:08:59 +0530

    Added : solve

--- EmpWageBuilderProject/src/com/Capg/EmpWageBuilderProject.java
+++ EmpWageBuilderProject/src/com/Capg/EmpWageBuilderProject.java
@@ -15,14 +15,17 @@ public class EmpWageBuilderProject {
      Random r = new Random();
      int empCheck =  r.nextInt(3) ;
      
-     if(empCheck == IS_FULL_TIME)
+     switch(empCheck) {
+     case IS_FULL_TIME:
              empHrs = 8;
-     else if (empCheck == IS_PART_TIME)
+             break;
+     case IS_PART_TIME:
              empHrs = 4;
-     else
+             break;
+     default:
     	     empHrs = 0;
      
-     
+     }
      empWage = empHrs * EMP_RATE_PER_HOUR;
      System.out.println("Emp Wage: " + empWage);
 	}
