5e043fe22231a6357e21cd348385b59d82820470
adityak-bit <adityajsr1998@gmail.com>
2020-09-29 12:12:44 +0530

    Added : calcuted wages for a month

--- EmpWageBuilderProject/src/com/Capg/EmpWageBuilderProject.java
+++ EmpWageBuilderProject/src/com/Capg/EmpWageBuilderProject.java
@@ -7,11 +7,13 @@ public class EmpWageBuilderProject {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     int empHrs = 0, empWage = 0;
+     int empHrs = 0, empWage = 0, totalEmpWage = 0;
      
+     for(int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
      Random r = new Random();
      int empCheck =  r.nextInt(3) ;
      
@@ -27,7 +29,9 @@ public class EmpWageBuilderProject {
      
      }
      empWage = empHrs * EMP_RATE_PER_HOUR;
+     totalEmpWage += empWage;
      System.out.println("Emp Wage: " + empWage);
 	}
-
+     System.out.println("Total Emp Wage: " + totalEmpWage);
+	}
 }
