23d3e8654bae30fabc61f5e4aef6bc4210a81c9d
adityak-bit <adityajsr1998@gmail.com>
2020-09-29 12:04:16 +0530

    Added : calculate daily employee wage

--- EmpWageBuilderProject/src/com/Capg/EmpWageBuilderProject.java
+++ EmpWageBuilderProject/src/com/Capg/EmpWageBuilderProject.java
@@ -5,17 +5,22 @@ import java.util.Random;
 public class EmpWageBuilderProject {
 
 	public static final int IS_FULL_TIME = 1;
+	public static final int EMP_RATE_PER_HOUR = 20;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+     int empHrs = 0, empWage = 0;
      
      Random r = new Random();
      int empCheck =  r.nextInt(2) ;
      
      if(empCheck == IS_FULL_TIME)
-             System.out.println("Employee is Present");
+             empHrs = 8;
      else
-    	     System.out.println("Employee is Abscent");
+    	     empHrs = 0;
+     
+     empWage = empHrs * EMP_RATE_PER_HOUR;
+     System.out.println("Emp Wage: " + empWage);
 	}
 
 }
