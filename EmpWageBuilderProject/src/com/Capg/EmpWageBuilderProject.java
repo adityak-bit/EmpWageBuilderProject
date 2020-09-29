b1682b51454d2c160ab0492eea69eb797b994d74
adityak-bit <adityajsr1998@gmail.com>
2020-09-29 12:21:02 +0530

    Ability to save the total wage for each company

--- EmpWageBuilderProject/src/com/Capg/EmpWageBuilderProject.java
+++ EmpWageBuilderProject/src/com/Capg/EmpWageBuilderProject.java
@@ -3,13 +3,25 @@ package com.Capg;
 import java.util.Random;
 
 public class EmpWageBuilderProject {
-
+	
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
+    private final String company;
+    private final int empRatePerHour;
+    private final int numOfWorkingDays;
+    private final int maxHoursPerMonth;
+    private int totalEmpWage;
+	
 
-	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-	int empHrs = 0, totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
-
+	public EmpWageBuilderProject(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+	this.company = company;
+	this.empRatePerHour = empRatePerHour;
+	this.numOfWorkingDays = numOfWorkingDays;
+	this.maxHoursPerMonth = maxHoursPerMonth;
+	}    
+	public void computeEmpWage() {
+	
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 	while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 	totalWorkingDays++;
 
@@ -31,12 +43,18 @@ public class EmpWageBuilderProject {
 	System.out.println("Day : " + totalWorkingDays + "empHrs : " + empHrs);
 	}
 	totalEmpWage = totalEmpHrs * empRatePerHour;
-	System.out.println("Total Emp Wage for Company: " +company+" is: "+ totalEmpWage);
-	return totalEmpWage;
 	}
 	
+	@Override
+	public String toString() {
+	return "Total Emp Wage for Company: " + company +" is: "+ totalEmpWage;
+	}
 	public static void main(String[] args) {
-     computeEmpWage("Reliance", 15, 25 , 100);
-     computeEmpWage("BigBazar", 20, 27 , 90);
+	EmpWageBuilderProject reliance = new EmpWageBuilderProject("Reliance", 15, 25, 100);
+	EmpWageBuilderProject bigBazar = new EmpWageBuilderProject("BigBazar", 20, 27, 90);
+    reliance.computeEmpWage();
+    System.out.println(reliance);
+    bigBazar.computeEmpWage();
+    System.out.println(bigBazar);
 	}
 }
